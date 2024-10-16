bb0bf64752b5d51c2470c8bbc8082192fcb1f519
Adam Vondracek <vondracekadam00@gmail.com>
2024-10-16 07:24:38 +0200

    variable naming and cleanup

--- src/main.zig
+++ src/main.zig
@@ -1,27 +1,28 @@
 const std = @import("std");
 const Integer = @import("integer.zig").Integer;
+const test_alloc = std.testing.allocator;
 
 test "APA Int: init/deinit" {
-    var x = try Integer.init(69, std.testing.allocator);
+    var x = try Integer.init(999, test_alloc);
     defer x.deinit();
 }
 
 test "APA Int: init form u64" {
-    var x = try Integer.from_u64(69696969, std.testing.allocator);
+    var x = try Integer.from_u64(123456, test_alloc);
     defer x.deinit();
 }
 
 test "APA Int: EXTEND" {
-    var x = try Integer.from_u64(69696969, std.testing.allocator);
+    var x = try Integer.from_u64(123456, test_alloc);
     defer x.deinit();
     try x.extend();
     x.print();
 }
 
 test "APA Int: add without overflow" {
-    var x = try Integer.from_u64(69, std.testing.allocator);
+    var x = try Integer.from_u64(100, test_alloc);
     defer x.deinit();
-    var y = try Integer.from_u64(420, std.testing.allocator);
+    var y = try Integer.from_u64(200, test_alloc);
     defer y.deinit();
     try x.add(y);
     x.print();
@@ -29,9 +30,9 @@ test "APA Int: add without overflow" {
 
 test "APA Int: add with overflow" {
     const big = std.math.maxInt(u64);
-    var x = try Integer.from_u64(big, std.testing.allocator);
+    var x = try Integer.from_u64(big, test_alloc);
     defer x.deinit();
-    var y = try Integer.from_u64(big, std.testing.allocator);
+    var y = try Integer.from_u64(big, test_alloc);
     defer y.deinit();
     try x.add(y);
     x.print();
